69198d505fbdb3eb7f637540eb0dbfcd4a98af57
Shreya Shanker <shreya.shanker.06@gmail.com>
2018-01-29 15:51:41 -0600

    added favorites and skaters pages

---
+++ FollowFigureSkating/app/src/main/java/vandy/cs4279/followfigureskating/SkatersActivity.java
@@ -0,0 +1,13 @@
+package vandy.cs4279.followfigureskating;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SkatersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skaters);
+    }
+}
