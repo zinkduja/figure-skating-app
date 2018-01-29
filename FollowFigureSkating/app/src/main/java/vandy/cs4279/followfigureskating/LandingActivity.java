10ca4d8582553a2d2d53d5c5eb397d1fcdd3c00b
Josie Zink-Duda <josephine.a.zink-duda@vanderbilt.edu>
2018-01-29 16:13:11 -0600

    starting nav

---
+++ FollowFigureSkating/app/src/main/java/vandy/cs4279/followfigureskating/LandingActivity.java
@@ -0,0 +1,47 @@
+package vandy.cs4279.followfigureskating;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class LandingActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_skaters:
+                    mTextMessage.setText(R.string.skaters);
+                    return true;
+                case R.id.navigation_following:
+                    mTextMessage.setText(R.string.following);
+                    return true;
+                case R.id.navigation_settings:
+                    mTextMessage.setText(R.string.settings);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
